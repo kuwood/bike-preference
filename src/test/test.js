0b78f2e48d60a89d3a1a064e7ee63f920e561cca
kuwood <p4nic510@gmail.com>
2016-09-27 16:08:08 -0600

    Hello world render with test

---
+++ src/test/test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import {should} from 'chai'
+should()
+import Main from '../components/Main.js'
+
+describe('Main component', () => {
+    it('Renders the Main component', () => {
+        const renderer = TestUtils.createRenderer()
+        renderer.render(<Main />)
+
+        const result = renderer.getRenderOutput()
+        result.props.children.props.children.should.equal('Hello World!')
+    })
+})
