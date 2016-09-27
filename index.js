93ccc1849553269fc27f63bb276d9c64929b0048
kuwood <p4nic510@gmail.com>
2016-09-27 15:14:52 -0600

    Configure JS environment, webpack and scripts

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './src/components/Main.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <Main />, document.getElementById('app'));
+});
