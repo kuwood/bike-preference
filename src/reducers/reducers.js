bbb2f527c20f893847fde750b86bfa9634900ae1
kuwood <p4nic510@gmail.com>
2016-10-10 15:38:56 -0600

    remove actions/action import (no longer exists)

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
-import * as actions from '../actions/actions'
 import initialState from './initialState'
 import destinationReducer from './destinationReducer'
 
