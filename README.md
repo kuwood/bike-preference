1a2d9252b1f96b707c27e7fed481510e17f5d5e0
kuwood <p4nic510@gmail.com>
2016-10-12 21:35:30 -0600

    Add build folder

--- README.md
+++
@@ -1,3 +0,0 @@
-#Bike Preference
-
-This app will supply you with a recommended bike route based on your time of travel and will include recommended clothing based on the weather during your travel times.
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-
-import bikeApp from './reducers/reducers'
-
-let store = createStore(bikeApp, applyMiddleware(thunk))
-
-export default store
