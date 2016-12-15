b21f4700cf9ff3306d6d0b91963ac162f8ddc3e4
Keith Underwood <keithunderwood510@gmail.com>
2016-12-14 18:19:29 -0700

    Change logo color, use import for smooth scroll polyfill

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import { polyfill } from 'smoothscroll-polyfill'
+import { polyfill } from 'smoothscroll-polyfill'
+const smooth = polyfill()
 
 import TextField from 'material-ui/TextField'
 import TimePicker from 'material-ui/TimePicker'
