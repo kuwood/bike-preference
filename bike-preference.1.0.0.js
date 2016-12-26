8d27b0b65c45e1905f8823de41cfa2d740d7a831
Keith Underwood <keithunderwood510@gmail.com>
2016-12-26 13:49:10 -0800

    Add jsdom to handle window object for tests

--- bike-preference.1.0.0.js
+++ bike-preference.1.0.0.js
@@ -46069,7 +46069,6 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
 	var smooth = (0, _smoothscrollPolyfill.polyfill)();
-	// require('smoothscroll-polyfill').polyfill()
 	
 	var SearchForm = exports.SearchForm = function (_React$Component) {
 	    _inherits(SearchForm, _React$Component);
